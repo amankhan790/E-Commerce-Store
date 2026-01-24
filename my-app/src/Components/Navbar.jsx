56cea2c9da2b59941ffc92f402687b682f9e213e
amankhan790 <amankassar790@gmail.com>
2026-01-24 19:56:22 +0530

    started my project

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaRegUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between mt-5 w-full items-center sticky p-4 bg-[#E9EBED] rounded-full">
+      <h2 className="text-2xl">Aman Tech</h2>
+      <div className="">
+        <ul className="flex gap-5">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">Product</li>
+          <li className="cursor-pointer">Category</li>
+        </ul>
+      </div>
+      <div className="flex items-center gap-10">
+        <div className="flex items-center">
+          <FaShoppingCart className="mr-2 text-2xl" />
+          <span className="cursor-pointer">Cart</span>
+        </div>
+        <div className="flex items-center">
+          <FaRegUser className="mr-2 text-2xl" />
+          <button className="cursor-pointer">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
