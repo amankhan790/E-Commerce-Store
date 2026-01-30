32aa40f1d596da7be662d194d1124dcb893f28eb
amankhan790 <amankassar790@gmail.com>
2026-01-30 22:15:19 +0530

    fixed category section bug

--- my-app/src/Components/Category.jsx
+++ my-app/src/Components/Category.jsx
@@ -1,97 +1,30 @@
 import React from "react";
-import {categoryImages} from "../assets/assets";
+import { categoryImages } from "../assets/assets";
 
 const Category = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold items-start mt-10 mb-10 text-[var(--text-color)]">
+      <h1 className="text-3xl font-bold items-center mt-15 mb-10 text-[var(--text-color)]">
         Shop by Category
       </h1>
-      <div className="flex justify-center gap-5 mb-10 ">
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.All})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-2xl font-bold text-[var(--text-color)] px-15 rounded-2xl relative top-30">
-            All
+      <div className="flex flex-wrap justify-between gap-5 mb-10 ">
+        {Object.entries(categoryImages).map(([name, src]) => (
+          <div
+            key={name}
+            className="w-full sm:w-1/4 md:w-1/3 lg:w-1/6 flex flex-col items-center cursor-pointer hover:scale-105 transform transition duration-300"
+          >
+            <div className="w-full h-30 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] relative overflow-hidden">
+              <img
+                src={src}
+                alt={name}
+                className="w-full h-full object-cover rounded-2xl"
+              />
+            </div>
+            <div className="text-2xl font-bold text-[var(--text-color)] mt-3">
+              {name}
+            </div>
           </div>
-        </div>
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.Electronics})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-xl font-semibold text-[var(--text-color)]  px-10 rounded-2xl relative top-30">
-            Electronics
-          </div>
-        </div>
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.Accessories})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-xl font-semibold text-[var(--text-color)] px-10 rounded-2xl relative top-30">
-            Accessories
-          </div>
-        </div>
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.Clothing})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-xl font-semibold text-[var(--text-color)] px-10 rounded-2xl relative top-30">
-            Clothing
-          </div>
-        </div>
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.Home})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-xl font-semibold text-[var(--text-color)] px-10 rounded-2xl relative top-30">
-            Home
-          </div>
-        </div>
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.Fashion})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-xl font-semibold text-[var(--text-color)] px-10 rounded-2xl relative top-30">
-            Fashion
-          </div>
-        </div>
-        <div
-          className="w-1/4 h-48 bg-[#E9EBED] rounded-2xl border-2 border-[var(--text-color)] flex items-center justify-center cursor-pointer hover:scale-105 transform transition duration-300"
-          style={{
-            backgroundImage: `url(${categoryImages.Sports})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="text-xl font-semibold text-[var(--text-color)] px-10 rounded-2xl relative top-30">
-            Sports
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
