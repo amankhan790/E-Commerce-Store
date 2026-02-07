8128a159bc2c002c21fb29df976f61ac93b7d7ae
amankhan790 <amankassar790@gmail.com>
2026-02-07 12:57:55 +0530

    built footer for ecommerce store

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
+import { FaFacebookSquare } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>
-       <div>
-        <h1>Ak Tech</h1>
-        <p></p>
-       </div>
-    </div>
-  )
-}
+    <>
+      <hr className="border-1 bg-[--text-color] mb-5 mt-15" />
+      <div className="bg-white-200 w-full h-7xl flex justify-between gap-20 mt-10 mb-10">
+        <div>
+          <h1 className="text-xl font-bold mb-5">Ak Tech</h1>
+          <p>Smart Shopping Made Simple.</p>
+          <p></p>
+        </div>
 
-export default Footer
+        <div>
+          <h1 className="text-xl font-bold mb-5">Shop</h1>
+          <ul className="flex flex-col gap-2">
+            <li className="cursor-pointer hover:font-bold">All Product</li>
+            <li className="cursor-pointer hover:font-bold">Electronics</li>
+            <li className="cursor-pointer hover:font-bold">Clothing</li>
+            <li className="cursor-pointer hover:font-bold">Accessories</li>
+          </ul>
+        </div>
+
+        <div>
+          <h1 className="text-xl font-bold mb-5">Company</h1>
+          <ul className="flex flex-col gap-2">
+            <li className="cursor-pointer hover:font-bold">About Us</li>
+            <li className="cursor-pointer hover:font-bold">Careers</li>
+            <li className="cursor-pointer hover:font-bold">Contact</li>
+            <li className="cursor-pointer hover:font-bold">Support</li>
+          </ul>
+        </div>
+      </div>
+
+      <hr className="border-1 bg-[--text-color] mb-5" />
+
+      <div className="text-sm text-[--text-color] flex justify-between items-center pb-10">
+        &copy; {new Date().getFullYear()} Ak Tech. All rights reserved.
+        <ul className="flex gap-5 text-xl">
+          <l className="cursor-pointer hover:opacity-70 transition-all">
+            <FaFacebookSquare />
+          </l>
+          <l className="cursor-pointer hover:opacity-70 transition-all">
+            <GrInstagram />
+          </l>
+          <l className="cursor-pointer hover:opacity-70 transition-all">
+            <FaXTwitter />
+          </l>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
