24b0df7df1f192b0b12de29914c780a650929f60
amankhan790 <amankassar790@gmail.com>
2026-02-14 19:01:46 +0530

    fixes navbar responsiveness

--- my-app/src/Components/Trending.jsx
+++ my-app/src/Components/Trending.jsx
@@ -9,7 +9,7 @@ const Trending = ({}) => {
         Browse Products
       </h1>
       <div>
-        <div className="grid grid-cols-4 gap-5 h-auto">
+        <div className="grid xl:grid-cols-3 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 h-auto">
           {AllProducts.map((product) => (
             <div
               key={product.id}
