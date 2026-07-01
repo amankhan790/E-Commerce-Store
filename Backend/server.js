6d0780d796a85c0a6f36b516934fe06dd61d4fad
amankhan790 <amankassar790@gmail.com>
2026-07-01 20:50:05 +0530

    chore: initial backend setup

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
